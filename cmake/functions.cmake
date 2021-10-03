1ab71fef981f933d5580d12742c5f8ffa6995d96
devcareer00 <fldevcareer0@gmail.com>
2021-10-03 18:09:22 +0200

    Added functions.cmake and transaction data test

---
+++ cmake/functions.cmake
@@ -0,0 +1,68 @@
+function(disable_clang_tidy target)
+  set_target_properties(${target} PROPERTIES
+      C_CLANG_TIDY ""
+      CXX_CLANG_TIDY ""
+      )
+endfunction()
+
+function(addtest test_name)
+  add_executable(${test_name} ${ARGN})
+  addtest_part(${test_name} ${ARGN})
+  target_link_libraries(${test_name}
+      GTest::gtest_main
+      GTest::gmock_main
+      )
+  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/xunit)
+  set(xml_output "--gtest_output=xml:${CMAKE_BINARY_DIR}/xunit/xunit-${test_name}.xml")
+  add_test(
+      NAME ${test_name}
+      COMMAND $<TARGET_FILE:${test_name}> ${xml_output}
+  )
+  set_target_properties(${test_name} PROPERTIES
+      RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/test_bin
+      ARCHIVE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test_lib
+      LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test_lib
+      )
+  disable_clang_tidy(${test_name})
+
+  if(FORCE_MULTILE)
+    set_target_properties(${test_name} PROPERTIES LINK_FLAGS "${MULTIPLE_OPTION}")
+  endif()
+endfunction()
+
+function(addtest_part test_name)
+  if (POLICY CMP0076)
+    cmake_policy(SET CMP0076 NEW)
+  endif ()
+  target_sources(${test_name} PUBLIC
+      ${ARGN}
+      )
+  target_link_libraries(${test_name}
+      GTest::gtest
+      )
+endfunction()
+
+# conditionally applies flag.
+function(add_flag flag)
+  check_cxx_compiler_flag(${flag} FLAG_${flag})
+  if (FLAG_${flag} EQUAL 1)
+    add_compile_options(${flag})
+  endif ()
+endfunction()
+
+function(print)
+  message(STATUS "[${CMAKE_PROJECT_NAME}] ${ARGV}")
+endfunction()
+
+### geniussdk_install should be called right after add_library(target)
+function(geniussdk_install target)
+    install(TARGETS ${target} EXPORT GeniusSDKTargets
+        LIBRARY       DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        ARCHIVE       DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME       DESTINATION ${CMAKE_INSTALL_BINDIR}
+        INCLUDES      DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+        PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+        FRAMEWORK     DESTINATION ${CMAKE_INSTALL_PREFIX}
+        BUNDLE        DESTINATION ${CMAKE_INSTALL_BINDIR}
+        )
+endfunction()
