080912f61855e67b8af1a312e5ff1bf09ef9c7fd
devcareer00 <fldevcareer0@gmail.com>
2021-11-22 23:01:45 +0100

    Use relative path of thirdparty dir if not definied

--- build/CommonBuildParameters.cmake
+++ build/CommonBuildParameters.cmake
@@ -1,21 +1,31 @@
 # --------------------------------------------------------
 # define third party directory
-if (NOT THIRDPARTY_DIR)
-  message(FATAL_ERROR "THIRDPARTY_DIR not found")
+if (NOT DEFINED THIRDPARTY_DIR)
+  print("Setting default third party directory")
+  set(THIRDPARTY_DIR "${CMAKE_CURRENT_LIST_DIR}/../../thirdparty")
+  ## get absolute path
+  cmake_path(SET THIRDPARTY_DIR NORMALIZE "${THIRDPARTY_DIR}")
 endif()
-print("THIRDPARTY BUILD DIR: ${THIRDPARTY_DIR}")
+
+if (NOT DEFINED THIRDPARTY_BUILD_DIR)
+  print("Setting third party build directory default")
+  set(THIRDPARTY_BUILD_DIR "${THIRDPARTY_DIR}/build/${CMAKE_SYSTEM_NAME}/${CMAKE_BUILD_TYPE}")
+endif()
+
+print("THIRDPARTY BUILD DIR: ${THIRDPARTY_BUILD_DIR}")
+print("THIRDPARTY SRC DIR: ${THIRDPARTY_DIR}")
 
 # --------------------------------------------------------
 # Set config of GTest
-set(GTest_DIR "${THIRDPARTY_DIR}/GTest/lib/cmake/GTest")
-set(GTest_INCLUDE_DIR "${THIRDPARTY_DIR}/GTest/include")
+set(GTest_DIR "${THIRDPARTY_BUILD_DIR}/GTest/lib/cmake/GTest")
+set(GTest_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/GTest/include")
 find_package(GTest CONFIG REQUIRED)
 include_directories(${GTest_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of protobuf project
-set(Protobuf_DIR "${THIRDPARTY_DIR}/grpc/lib/cmake/protobuf")
-set(Protobuf_INCLUDE_DIR "${THIRDPARTY_DIR}/grpc/include/google/protobuf")
+set(Protobuf_DIR "${THIRDPARTY_BUILD_DIR}/grpc/lib/cmake/protobuf")
+set(Protobuf_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/grpc/include/google/protobuf")
 if (${CMAKE_SYSTEM_NAME} STREQUAL "iOS")
   set(Protobuf_DIR  "${iOS_Protobuf_DIR}")
   set(Protobuf_INCLUDE_DIR "${iOS_Protobuf_INCLUDE_DIR}")
@@ -23,15 +33,15 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "iOS")
 endif()
 if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
   # TODO: Check grpc install on Windows
-  set(Protobuf_DIR  "${THIRDPARTY_DIR}/grpc/build/third_party/protobuf/cmake")
-  set(Protobuf_INCLUDE_DIR "${THIRDPARTY_DIR}/grpc/build/third_party/protobuf/include/google/protobuf")
+  set(Protobuf_DIR  "${THIRDPARTY_BUILD_DIR}/grpc/build/third_party/protobuf/cmake")
+  set(Protobuf_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/grpc/build/third_party/protobuf/include/google/protobuf")
 endif()
 
 find_package(Protobuf CONFIG REQUIRED )
 include_directories(${Protobuf_INCLUDE_DIR})
 
 if (NOT DEFINED PROTOC_EXECUTABLE)
-  set(PROTOC_EXECUTABLE "${THIRDPARTY_DIR}/grpc/bin/protoc${CMAKE_EXECUTABLE_SUFFIX}")
+  set(PROTOC_EXECUTABLE "${THIRDPARTY_BUILD_DIR}/grpc/bin/protoc${CMAKE_EXECUTABLE_SUFFIX}")
 endif()
 
 set(Protobuf_PROTOC_EXECUTABLE ${PROTOC_EXECUTABLE} CACHE PATH "Initial cache" FORCE)
@@ -55,7 +65,7 @@ include(${PROJECT_ROOT}/cmake/functions.cmake)
 
 # --------------------------------------------------------
 # Set config of openssl project
-set(OPENSSL_DIR "${THIRDPARTY_DIR}/openssl/build/${CMAKE_SYSTEM_NAME}${ABI_SUBFOLDER_NAME}")
+set(OPENSSL_DIR "${THIRDPARTY_BUILD_DIR}/openssl/build/${CMAKE_SYSTEM_NAME}${ABI_SUBFOLDER_NAME}")
 set(OPENSSL_USE_STATIC_LIBS ON)
 set(OPENSSL_MSVC_STATIC_RT ON)
 set(OPENSSL_ROOT_DIR "${OPENSSL_DIR}")
@@ -69,64 +79,64 @@ include_directories(${OPENSSL_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of rocksdb
-set(RocksDB_DIR "${THIRDPARTY_DIR}/rocksdb/lib/cmake/rocksdb")
-set(RocksDB_INCLUDE_DIR "${THIRDPARTY_DIR}/rocksdb/include")
+set(RocksDB_DIR "${THIRDPARTY_BUILD_DIR}/rocksdb/lib/cmake/rocksdb")
+set(RocksDB_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/rocksdb/include")
 find_package(RocksDB CONFIG REQUIRED)
 include_directories(${RocksDB_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of Microsoft.GSL
-set(GSL_INCLUDE_DIR "${THIRDPARTY_DIR}/GSL/include")
+set(GSL_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/GSL/include")
 include_directories(${GSL_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of spdlog v1.4.2
-set(spdlog_DIR "${THIRDPARTY_DIR}/spdlog/lib/cmake/spdlog")
-set(spdlog_INCLUDE_DIR "${THIRDPARTY_DIR}/spdlog/include")
+set(spdlog_DIR "${THIRDPARTY_BUILD_DIR}/spdlog/lib/cmake/spdlog")
+set(spdlog_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/spdlog/include")
 find_package(spdlog CONFIG REQUIRED)
 include_directories(${spdlog_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of  tsl_hat_trie
-set(tsl_hat_trie_DIR "${THIRDPARTY_DIR}/hat-trie/lib/cmake/tsl_hat_trie")
-set(tsl_hat_trie_INCLUDE_DIR "${THIRDPARTY_DIR}/hat-trie/include")
+set(tsl_hat_trie_DIR "${THIRDPARTY_BUILD_DIR}/hat-trie/lib/cmake/tsl_hat_trie")
+set(tsl_hat_trie_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/hat-trie/include")
 find_package(tsl_hat_trie CONFIG REQUIRED)
 include_directories(${tsl_hat_trie_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of libp2p
-set(libp2p_DIR "${THIRDPARTY_DIR}/libp2p/lib/cmake/libp2p")
-set(libp2p_LIBRARY_DIR "${THIRDPARTY_DIR}/libp2p/lib")
-set(libp2p_INCLUDE_DIR    "${THIRDPARTY_DIR}/libp2p/include")
+set(libp2p_DIR "${THIRDPARTY_BUILD_DIR}/libp2p/lib/cmake/libp2p")
+set(libp2p_LIBRARY_DIR "${THIRDPARTY_BUILD_DIR}/libp2p/lib")
+set(libp2p_INCLUDE_DIR    "${THIRDPARTY_BUILD_DIR}/libp2p/include")
 find_package(libp2p CONFIG REQUIRED)
 include_directories(${libp2p_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of ipfs-lite-cpp
-set(ipfs-lite-cpp_DIR "${THIRDPARTY_DIR}/ipfs-lite-cpp/cmake/ipfs-lite-cpp")
-set(ipfs-lite-cpp_INCLUDE_DIR "${THIRDPARTY_DIR}/ipfs-lite-cpp/include")
-set(ipfs-lite-cpp_LIB_DIR "${THIRDPARTY_DIR}/ipfs-lite-cpp/lib")
-set(CBOR_INCLUDE_DIR "${THIRDPARTY_DIR}/ipfs-lite-cpp/include/deps/tinycbor/src")
+set(ipfs-lite-cpp_DIR "${THIRDPARTY_BUILD_DIR}/ipfs-lite-cpp/cmake/ipfs-lite-cpp")
+set(ipfs-lite-cpp_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/ipfs-lite-cpp/include")
+set(ipfs-lite-cpp_LIB_DIR "${THIRDPARTY_BUILD_DIR}/ipfs-lite-cpp/lib")
+set(CBOR_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/ipfs-lite-cpp/include/deps/tinycbor/src")
 find_package(ipfs-lite-cpp CONFIG REQUIRED)
 include_directories(${ipfs-lite-cpp_INCLUDE_DIR} ${CBOR_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of ipfs-pubsub
-set(ipfs-pubsub_INCLUDE_DIR "${THIRDPARTY_DIR}/ipfs-pubsub/include")
-set(ipfs-pubsub_DIR "${THIRDPARTY_DIR}/ipfs-pubsub/lib/cmake/ipfs-pubsub")
+set(ipfs-pubsub_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/ipfs-pubsub/include")
+set(ipfs-pubsub_DIR "${THIRDPARTY_BUILD_DIR}/ipfs-pubsub/lib/cmake/ipfs-pubsub")
 find_package(ipfs-pubsub CONFIG REQUIRED)
 include_directories(${ipfs-pubsub_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of Boost.DI
-set(Boost.DI_INCLUDE_DIR "${THIRDPARTY_DIR}/Boost.DI/include")
-set(Boost.DI_DIR "${THIRDPARTY_DIR}/Boost.DI/lib/cmake/Boost.DI")
+set(Boost.DI_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/Boost.DI/include")
+set(Boost.DI_DIR "${THIRDPARTY_BUILD_DIR}/Boost.DI/lib/cmake/Boost.DI")
 find_package(Boost.DI CONFIG REQUIRED)
 include_directories(${Boost.DI_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of Boost project
-set(_BOOST_ROOT "${THIRDPARTY_DIR}/boost/build/${CMAKE_SYSTEM_NAME}${ABI_SUBFOLDER_NAME}")
+set(_BOOST_ROOT "${THIRDPARTY_BUILD_DIR}/boost/build/${CMAKE_SYSTEM_NAME}${ABI_SUBFOLDER_NAME}")
 set(Boost_LIB_DIR "${_BOOST_ROOT}/lib")
 set(Boost_INCLUDE_DIR "${_BOOST_ROOT}/include/boost-1_72")
 set(Boost_DIR "${Boost_LIB_DIR}/cmake/Boost-1.72.0")
@@ -164,56 +174,55 @@ include_directories(${Boost_INCLUDE_DIRS})
 
 # --------------------------------------------------------
 # Set config of ed25519
-set(ed25519_DIR "${THIRDPARTY_DIR}/ed25519/lib/cmake/ed25519")
-set(ed25519_INCLUDE_DIR "${THIRDPARTY_DIR}/ed25519/include")
+set(ed25519_DIR "${THIRDPARTY_BUILD_DIR}/ed25519/lib/cmake/ed25519")
+set(ed25519_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/ed25519/include")
 find_package(ed25519 CONFIG REQUIRED)
 include_directories(${ed25519_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of sr25519-donna
-set(sr25519-donna_DIR "${THIRDPARTY_DIR}/sr25519-donna/lib/cmake/sr25519-donna")
-set(sr25519-donna_INCLUDE_DIR "${THIRDPARTY_DIR}/sr25519-donna/include")
+set(sr25519-donna_DIR "${THIRDPARTY_BUILD_DIR}/sr25519-donna/lib/cmake/sr25519-donna")
+set(sr25519-donna_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/sr25519-donna/include")
 find_package(sr25519-donna CONFIG REQUIRED)
 include_directories(${sr25519-donna_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set RapidJSON config path
-set(RapidJSON_DIR "${THIRDPARTY_DIR}/rapidjson/lib/cmake/RapidJSON")
-set(RapidJSON_INCLUDE_DIR "${THIRDPARTY_DIR}/rapidjson/include")
+set(RapidJSON_DIR "${THIRDPARTY_BUILD_DIR}/rapidjson/lib/cmake/RapidJSON")
+set(RapidJSON_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/rapidjson/include")
 find_package(RapidJSON CONFIG REQUIRED)
 include_directories(${RapidJSON_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set jsonrpc-lean include path
-set(jsonrpc_lean_INCLUDE_DIR "${_THIRDPARTY_DIR}/jsonrpc-lean/include")
+set(jsonrpc_lean_INCLUDE_DIR "${THIRDPARTY_DIR}/jsonrpc-lean/include")
 include_directories(${jsonrpc_lean_INCLUDE_DIR})
 
 # --------------------------------------------------------
-# Set config of binaryen
-set(binaryen_INCLUDE_DIR "${THIRDPARTY_DIR}/binaryen/include")
-set(binaryen_LIBRARIES "${THIRDPARTY_DIR}/binaryen/lib")
-set(binaryen_DIR "${THIRDPARTY_DIR}/binaryen/lib/cmake/binaryen")
-find_package(binaryen CONFIG REQUIRED)
-include_directories(${binaryen_INCLUDE_DIR} ${binaryen_INCLUDE_DIR}/binaryen)
-
-# --------------------------------------------------------
 # Set config of secp256k1
-set(libsecp256k1_INCLUDE_DIR "${THIRDPARTY_DIR}/libsecp256k1/include")
-set(libsecp256k1_LIBRARY_DIR "${THIRDPARTY_DIR}/libsecp256k1/lib")
-set(libsecp256k1_DIR "${THIRDPARTY_DIR}/libsecp256k1/lib/cmake/libsecp256k1")
+set(libsecp256k1_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/libsecp256k1/include")
+set(libsecp256k1_LIBRARY_DIR "${THIRDPARTY_BUILD_DIR}/libsecp256k1/lib")
+set(libsecp256k1_DIR "${THIRDPARTY_BUILD_DIR}/libsecp256k1/lib/cmake/libsecp256k1")
 find_package(libsecp256k1 CONFIG REQUIRED)
 include_directories(${libsecp256k1_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of xxhash
-set(xxhash_INCLUDE_DIR "${THIRDPARTY_DIR}/xxhash/include")
-set(xxhash_LIBRARY_DIR "${THIRDPARTY_DIR}/xxhash/lib")
-set(xxhash_DIR "${THIRDPARTY_DIR}/xxhash/lib/cmake/xxhash")
+set(xxhash_INCLUDE_DIR "${THIRDPARTY_BUILD_DIR}/xxhash/include")
+set(xxhash_LIBRARY_DIR "${THIRDPARTY_BUILD_DIR}/xxhash/lib")
+set(xxhash_DIR "${THIRDPARTY_BUILD_DIR}/xxhash/lib/cmake/xxhash")
 find_package(xxhash CONFIG REQUIRED)
 include_directories(${xxhash_INCLUDE_DIR})
 
 # --------------------------------------------------------
 # Set config of SuperGenius project
+if (NOT DEFINED SuperGenius_DIR)
+  print("Setting SuperGenius build directory default")
+  set(SUPERGENIUS_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/../../SuperGenius")
+  set(SuperGenius_DIR "${SUPERGENIUS_SRC_DIR}/build/${CMAKE_SYSTEM_NAME}/${CMAKE_BUILD_TYPE}/SuperGenius/lib/cmake/SuperGenius/")
+endif()
+print("SuperGenius_DIR: ${SuperGenius_DIR}")
+
 find_package(SuperGenius CONFIG REQUIRED)
 include_directories(${SuperGenius_INCLUDE_DIR})
 
