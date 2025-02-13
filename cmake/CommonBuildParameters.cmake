1da876cccd3ab4d3c418f0a186b99990a54a9473
Eduardo Menges Mattje <eduardomattje@gmail.com>
2025-02-13 14:15:49 -0300

    Debug

--- cmake/CommonBuildParameters.cmake
+++ cmake/CommonBuildParameters.cmake
@@ -222,6 +222,7 @@ set(boost_chrono_DIR "${Boost_LIB_DIR}/cmake/boost_chrono-${BOOST_VERSION}")
 set(boost_log_DIR "${Boost_LIB_DIR}/cmake/boost_log-${BOOST_VERSION}")
 set(boost_log_setup_DIR "${Boost_LIB_DIR}/cmake/boost_log_setup-${BOOST_VERSION}")
 set(boost_thread_DIR "${Boost_LIB_DIR}/cmake/boost_thread-${BOOST_VERSION}")
+set(boost_unit_test_framework_DIR "${Boost_LIB_DIR}/cmake/boost_unit_test_framework-${BOOST_VERSION}")
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_LIBS ON)
 set(Boost_NO_SYSTEM_PATHS ON)
@@ -236,6 +237,7 @@ if(SGNS_STACKTRACE_BACKTRACE)
         add_definitions(-DBOOST_STACKTRACE_BACKTRACE_INCLUDE_FILE=${BACKTRACE_INCLUDE})
     endif()
 endif()
+
 
 # header only libraries must not be added here
 find_package(Boost REQUIRED COMPONENTS date_time filesystem random regex system thread log log_setup program_options unit_test_framework)
@@ -432,7 +434,9 @@ set(zkLLVM_INCLUDE_DIR "${ZKLLVM_DIR}/zkLLVM/include")
 
 # Set config of llvm
 set(LLVM_DIR "${ZKLLVM_DIR}/zkLLVM/lib/cmake/llvm")
+message(WARNING "${LLVM_DIR}")
 find_package(LLVM CONFIG REQUIRED)
+message(WARNING "${LLVM_DIR}")
 
 # Set config of SuperGenius project
 if(NOT DEFINED SUPERGENIUS_SRC_DIR)
